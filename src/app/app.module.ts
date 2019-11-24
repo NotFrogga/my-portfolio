709184ca3488313553c4c70708b76f9f24b5c6ca
Hichame <chamou76@gmail.com>
2019-11-24 10:05:24 +0100

    creation of core components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MenuBarComponent } from "./menu-bar/menu-bar.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { FooterComponent } from "./footer/footer.component";
+import { ProjectsPageComponent } from "./projects-page/projects-page.component";
+import { ProjectComponent } from "./project/project.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuBarComponent,
+    HomePageComponent,
+    FooterComponent,
+    ProjectsPageComponent,
+    ProjectComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
