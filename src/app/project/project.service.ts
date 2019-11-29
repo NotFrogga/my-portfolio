efb395e8329af4a69759f5e2947095b4c36d1c18
h.oukdim <h.oukdim@VIV-F1607>
2019-11-29 17:55:19 +0100

    fix(project-component): fixed images and added link property for projects

--- src/app/project/project.service.ts
+++ src/app/project/project.service.ts
@@ -8,20 +8,23 @@ export class ProjectService {
 				title: "Flip the cards",
 				description: `Web app game in which you start with some cards face up, some face down. You can remove any face up card, but the adjacent cards, if any, will flip.
 				The goal is to successfully remove every card.`,
-				imgUrl: "https://images.freeimages.com/images/large-previews/962/cards-1425279.jpg",
-				programmingSkills: "HTML, CSS, Javascript"
+				imgUrl: "assets/img/cards.jpg",
+				programmingSkills: "HTML, CSS, Javascript",
+				link:"www.google.com"
 			},
 			1: {
 				title: "My Secret Diary",
 				description: "Web app in which you use a login and password to connect to your secret diary. The diary is updated automatically and is stored in a SQL database.",
-				imgUrl: "https://images.freeimages.com/images/large-previews/53d/diary-page-1240501.jpg",
-				programmingSkills: "HTML, Bootstrap, Javascript, PHP, MySQL"
+				imgUrl: "assets/img/diary-page.jpg",
+				programmingSkills: "HTML, Bootstrap, Javascript, PHP, MySQL",
+				link:"https://www.google.com"
 			},
 			2: {
 				title: "Weather Forecast",
 				description: "Web app in which you can see a short forecast a city by using OpenWeatherMap API",
-				imgUrl: "https://images.freeimages.com/images/large-previews/f94/sky-sun-behind-clouds-sunburst-1337408.jpg",
-				programmingSkills: "HTML, Bootstrap, Javascript, HTTP REST"
+				imgUrl: "assets/img/clouds.jpg",
+				programmingSkills: "HTML, Bootstrap, Javascript, HTTP REST",
+				link:"https://www.google.com"
 			},
 		});
 	}
