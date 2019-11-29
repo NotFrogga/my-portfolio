3352478d4bdc3443e8578529d42c1ddc0c5bf17b
Hichame <chamou76@gmail.com>
2019-11-29 21:07:21 +0100

    fix(project-component): links to projects websites

--- src/app/project/project.service.ts
+++ src/app/project/project.service.ts
@@ -1,31 +1,32 @@
-import { ProjectComponent } from './project.component'; 
+import { ProjectComponent } from "./project.component";
 
 export class ProjectService {
-
-	getProjects() {
-		return ({
-			0: {
-				title: "Flip the cards",
-				description: `Web app game in which you start with some cards face up, some face down. You can remove any face up card, but the adjacent cards, if any, will flip.
+  getProjects() {
+    return {
+      0: {
+        title: "Flip the cards",
+        description: `Web app game in which you start with some cards face up, some face down. You can remove any face up card, but the adjacent cards, if any, will flip.
 				The goal is to successfully remove every card.`,
-				imgUrl: "assets/img/cards.jpg",
-				programmingSkills: "HTML, CSS, Javascript",
-				link:"www.google.com"
-			},
-			1: {
-				title: "My Secret Diary",
-				description: "Web app in which you use a login and password to connect to your secret diary. The diary is updated automatically and is stored in a SQL database.",
-				imgUrl: "assets/img/diary-page.jpg",
-				programmingSkills: "HTML, Bootstrap, Javascript, PHP, MySQL",
-				link:"https://www.google.com"
-			},
-			2: {
-				title: "Weather Forecast",
-				description: "Web app in which you can see a short forecast a city by using OpenWeatherMap API",
-				imgUrl: "assets/img/clouds.jpg",
-				programmingSkills: "HTML, Bootstrap, Javascript, HTTP REST",
-				link:"https://www.google.com"
-			},
-		});
-	}
+        imgUrl: "assets/img/cards.jpg",
+        programmingSkills: "HTML, CSS, Javascript",
+        link: "www.lehich.com/projects/flipthecards/"
+      },
+      1: {
+        title: "My Secret Diary",
+        description:
+          "Web app in which you use a login and password to connect to your secret diary. The diary is updated automatically and is stored in a SQL database.",
+        imgUrl: "assets/img/diary-page.jpg",
+        programmingSkills: "HTML, Bootstrap, Javascript, PHP, MySQL",
+        link: "www.lehich.com/projects/secretdiary/"
+      },
+      2: {
+        title: "Weather Forecast",
+        description:
+          "Web app in which you can see a short forecast a city by using OpenWeatherMap API",
+        imgUrl: "assets/img/clouds.jpg",
+        programmingSkills: "HTML, Bootstrap, Javascript, HTTP REST",
+        link: "www.lehich.com/projects/weatherforecast/"
+      }
+    };
+  }
 }
