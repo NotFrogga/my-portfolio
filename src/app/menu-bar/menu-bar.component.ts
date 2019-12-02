deee95776d7dc0a0fd2ebd523ce9b520ac0bf1f0
h.oukdim <h.oukdim@VIV-F1607>
2019-12-02 16:11:25 +0100

    feat(all html/css components): media mobile users

--- src/app/menu-bar/menu-bar.component.ts
+++ src/app/menu-bar/menu-bar.component.ts
@@ -6,14 +6,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-bar.component.scss']
 })
 export class MenuBarComponent implements OnInit {
-
+  randomAnimation : Array<string> = ["rubberBand","pulse","bounce", "shake", "wobble", "jackInTheBox", "swing", "tada"];
   constructor() { }
 
   ngOnInit() {
 	}
 	
+	/**
+	 * Scrolls view to bottom of website page
+	 *
+	 * @memberof MenuBarComponent
+	 */
 	scrollDown() {
 		window.scrollTo(0, document.body.scrollHeight);
 	}
 
+  /**
+	 * Selects a random index and get the value from that index
+	 *
+	 * @param {Array<any>} array
+	 * @memberof MenuBarComponent
+	 */
+	selectRandom(array : Array<any>) : any {
+		const lenght = array.length;
+		const ran = parseInt((Math.random()*lenght).toString(), 10);
+		return array[ran];
+	}
+
+	/**
+	 * Triggers logo animation on click
+	 *
+	 * @memberof MenuBarComponent
+	 */
+	logoAnimation() {
+		const animation = this.selectRandom(this.randomAnimation);
+		let element = document.querySelector("#logo");
+		element.classList.add('animated', animation);
+		element.addEventListener('animationend', () => element.classList.remove('animated', animation));
+	}
 }
